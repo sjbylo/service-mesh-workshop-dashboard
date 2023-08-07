b1df722e26b3b36f69383534fb8ec60b5aab9e4e
Stephen Bylo <stephenbylo@gmail.com>
2023-08-07 22:43:28 +0800

    Update

--- steve/go.sh
+++ steve/go.sh
@@ -2,8 +2,8 @@
 # Build all images for the workshop
 # BE SURE TO log in as admin user first!
 
-CON=5
-COUNT=10
+CON=5     # Number of concurrent sub-processes
+COUNT=10  # Total number of users
 [ "$1" ] && COUNT=$1
 [ "$2" ] && CON=$2
 
@@ -22,7 +22,6 @@ trap terminate_subprocesses EXIT
 set -e
 
 u=1
-#for u in {1..20}; do 
 for u in $(seq 1 $COUNT); do
 	echo Building images for user$u - See log file: /tmp/pre-build-images.user$u.log >&2
 	LF=/tmp/pre-build-images.user$u.log
